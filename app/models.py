5e7c3286135edd9f5ecbb4cd9ba19cdf165af369
tkg61 <icptkg@gmail.com>
2016-11-06 18:09:38 -0500

    Added db migrate and upgrade for db management
    added more items to model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,15 @@
 from app import db
 
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index= True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    location = db.Column(db.String(64), index=True)
+    address = db.Column(db.String(120), index=True)
+    apikey = db.Column(db.String(64), index=True, unique=True)
+
+
 
     def __repr__(self):
         return '<User %r>' % self.name
