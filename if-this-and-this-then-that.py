bdbfa3e658e979cb902d18da92226f94cef0be08
tkg61 <icptkg@gmail.com>
2016-11-06 16:09:32 -0500

    Initial commit
    Transferring code from scratch projects

---
+++ if-this-and-this-then-that.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def defaultpath():
+    return "This domain is currently not working. Please try back later."
+
+
+@app.route('/pf-api/v1.0/maker/location/<string:person>/<string:event>/<string:location>')
+@app.route('/pf-api/v1.0/maker/location/<string:person>/<string:event>/<string:location>/<path:coordinates>')
+def location_sms(person, event, location, coordinates=None):
+    action = person + "_" + event + "_" + location
+
+    location_update(person, event, location)
+
+    key = getkeyforevent(action)
+
+    if coordinates:
+        r = re.search('q=(.*)&z=', coordinates)
+        print r.group(1)
+        globals()[action](action, key, r.group(1))
+    else:
+        globals()[action](action, key)
+
+    # if key:
+    #     sendtoifttt(action, key)
+    #
+    # else:
+    #     print action, key
+    #     printlocation()
+
+    return "Something", 200
+
+
+if __name__ == '__main__':
+    app.run()
